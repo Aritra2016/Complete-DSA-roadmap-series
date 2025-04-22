0cc55f11798110393e13af066a1f60025d063c87
Aritra Khatua <aritrakhatua20@gmail.com>
2025-04-22 20:54:22 +0530

    prime

--- src/main/java/Method/numberPrime.java
+++ src/main/java/Method/numberPrime.java
@@ -8,7 +8,7 @@ public class numberPrime {
         for(int i=2; i<=n-1; i++){
             if(n%i==0){
                 isPrime=false;
-                break; //coming out of loop
+               break; //coming out of loop
             }
 
         }
@@ -18,6 +18,6 @@ public class numberPrime {
 
     public static void main(String[] args) {
 
-        System.out.println(primeCheck(17));
+        System.out.println(primeCheck(16));
     }
 }
