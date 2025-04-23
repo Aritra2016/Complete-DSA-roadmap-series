c00fb75f68ad3ca80f92cb058f2a7354d417c190
Aritra Khatua <aritrakhatua20@gmail.com>
2025-04-23 12:19:58 +0530

    prime

---
+++ src/main/java/Method/binaToDeci.java
@@ -0,0 +1,27 @@
+package Method;
+
+public class binaToDeci {
+
+    public static void binToDec(int binNum){
+        int pow=0;
+        int deci=0;
+
+        int myNum= binNum;
+
+        while(binNum > 0){
+            int lastDigit= binNum % 10;
+            deci= (int) (deci + lastDigit * (Math.pow(2,pow)));
+
+            pow++;
+            binNum=binNum /10;
+
+        }
+
+        System.out.println("Decimal of -> "+  myNum +" is ="+deci);
+    }
+    public static void main(String[] args) {
+
+        binToDec(101);
+
+    }
+}
