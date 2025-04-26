00d0973d4b8b3e323c6b5d54a6b5dd7274794d34
Aritra Khatua <aritrakhatua20@gmail.com>
2025-04-26 22:30:03 +0530

    prime

--- src/main/java/Array/removeDuplicate.java
+++ src/main/java/Array/removeDuplicate.java
@@ -7,7 +7,7 @@ public class removeDuplicate {
         for(int i=0; i< arr.length; i++){
             for(int j=i+1; j< arr.length; j++){
                 if(arr[i]==arr[j]){
-
+                     arr
                 }
             }
         }
